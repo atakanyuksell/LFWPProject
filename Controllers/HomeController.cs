4e91754ce9c18a708157f47b2a21a2782a3f5f02
atakanyuksell <atakanyukselis@hotmail.com>
2025-02-05 22:23:09 +0300

    Project refactored

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,34 +7,11 @@ namespace LawFirmWebPage.Controllers
 
 	public class HomeController : Controller
 	{
-
-		//Get Home/Login
 		[Route("/")]
-		[Route("[action]")]
-		[HttpGet]
-		public IActionResult Login()
+        public IActionResult page()
 		{
 			return View();
-		}
+        }
 
-		[HttpPost]
-		[Route("[action]")]
-		public IActionResult Login(LoginPerson person)
-		{
-			if (!ModelState.IsValid)
-			{
-				// Eğer model doğrulama hatalıysa tekrar Index view'ına döner
-				return RedirectToAction("Login", "Home");
-			}
-			if (person.Name == "abdhanabd" && person.Password == "081123")
-			{
-				// Giriş başarılıysa Login view'ına yönlendir
-				return View("Page"); // Page view'ını döndür
-
-			}
-
-			return View();
-		}
-
-	}
+    }
 }
